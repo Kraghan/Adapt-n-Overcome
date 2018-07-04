dbf062a533f641030c47d50344d53c88c22be376
Kraghan <madjojo70@live.fr>
2018-07-04 12:11:04 +0200

    Add entity and menu

---
+++ Assets/Scripts/Utils/UI/LockButtonWithField.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockButtonWithField : MonoBehaviour
+{
+    [SerializeField]
+    private InputField m_linkedField;
+    private Button m_buttonToLock;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_buttonToLock = GetComponent<Button>();
+        m_buttonToLock.interactable = false;
+    }
+	
+	public void CheckButton()
+    {
+        if (m_linkedField.text != "")
+            m_buttonToLock.interactable = true;
+        else
+            m_buttonToLock.interactable = false;
+    }
+}
